7d4f00d62de59b77f215f00647dbd829c8f7f231
aaronleon1 <aaron.leon21@gmail.com>
2020-09-10 23:23:57 -0400

    Implemented react-tabs in ProductDetails to view details/warranty/shipping information

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,43 +1,59 @@
-import React, {useState, Component} from 'react';
+import React, { useState, Component } from 'react';
 import './ProductDetails.css'
 import GraphicsCards from '../pages/GraphicsCards/GraphicsCards'
-import {Link} from 'react-router-dom'
-import {ProductConsumer} from '../context'
+import { Link } from 'react-router-dom'
+import { ProductConsumer } from '../context'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 
 
 
 class ProductDetails extends Component {
-    render(){
+    render() {
         return (
             <ProductConsumer>
                 {(value) => {
-                   const {id, productName, productImage, productPrice, details} = value.detailProduct
-                   return (
-                    <div className='detail-wrapper'>
-                    
-                    <div className='product-detail-wrapper'>
-                        <div className='prod-image'>
-                        <img src={productImage}  alt={productName}/>
+                    const { id, productName, productImage, productPrice, details } = value.detailProduct
+                    return (
+                        <div className='detail-wrapper'>
+
+                            <div className='product-detail-wrapper'>
+                                <div className='prod-image'>
+                                    <img src={productImage} alt={productName} />
+                                </div>
+                                <div className='prod-details'>
+                                    <p>{productName}</p>
+                                    <p>{productPrice}</p>
+                                    <button className='back-btn'><i class="fas fa-arrow-left" /> Back to Products</button>
+                                    <button className='add-to-cart'><i class="fas fa-cart-plus" /> Add to Cart</button>
+                                </div>
+                                <div className='prod-specs'>
+                                    <Tabs >
+                                        <TabList>
+                                            <Tab>Product Specifications</Tab>
+                                            <Tab>Warranty</Tab>
+                                            <Tab>Shipping</Tab>
+                                        </TabList>
+                                        <TabPanel>{details}</TabPanel>
+                                        <TabPanel><p>All products come standard with a 30-Day warranty unless marked otherwise.</p></TabPanel>
+                                        <TabPanel>
+                                            <ul>
+                                                <li>5-7 Business Days: $5.99</li>
+                                                <li>2 Days: 14.99</li>
+                                                <li>All orders over $399: Free 2-Day upgrade</li>
+                                            </ul>
+                                        </TabPanel>
+                                    </Tabs>
+                                </div>
+                            </div>
                         </div>
-                        <div className='prod-details'>
-                            <p>{productName}</p>
-                            <p>{productPrice}</p>
-                            <button className='back-btn'><i class="fas fa-arrow-left" /> Back to Products</button>
-                            <button className='add-to-cart'><i class="fas fa-cart-plus" /> Add to Cart</button>
-                        </div>
-                        <div className='prod-specs'>
-                            <h3>Product Specifications</h3>
-                            {details}
-                        </div>
-                    </div>
-                </div>
-                   )
+                    )
                 }}
             </ProductConsumer>
         )
     }
-    
+
 
 }
 
