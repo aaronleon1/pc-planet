6feda84566351c50f606416dd8de6ab2e1318af3
Aaron Leon <aaron.leon21@gmail.com>
2020-09-17 21:30:20 -0400

    Added firebase realtime database functionality, currently testing in default.js

--- src/components/pages/Default.js
+++ src/components/pages/Default.js
@@ -1,22 +1,40 @@
 import React, {Component} from 'react'
 import axios from 'axios'
 import './Default.css'
+import firebase from '../../firebase'
 
 class Default extends Component {
+    
+    state={
+        products: []
+    }
     componentDidMount(){
         axios.get('https://pc-planet-55aa4.firebaseio.com/graphics-cards.json')
         .then(response => {
-            console.log(response)
+            const fetchedProducts = []
+            for(let key in response.data){
+                fetchedProducts.push({
+                    ...response.data[key],
+                    prodID: key
+                })
+            }
+            this.setState({products: fetchedProducts})
+            
+            console.log(this.state.products)
         })
     }
     render() {
-        console.log(this.props)
+        const prodnames = this.state.products.map((n) =>{
+            return<p>{n.productName}</p>
+        })
+        var database = firebase.database();
         return(
             <div className='error-container'>
                 <div className='error-code'>
                     <h1>404</h1>
                     <h2>Page not found</h2>
                     <h3>The requested URL <span>{this.props.location.pathname} </span>was not found</h3>
+                    {prodnames}               
                 </div>
             </div>
         )
