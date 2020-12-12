64fe31f1649ea9bfe73efbce009927327eec3da8
aaronleon1 <aaron.leon21@gmail.com>
2020-12-11 23:26:14 -0500

    products page restyled

--- src/components/pages/NewProductsPage/NewProductsPage.js
+++ src/components/pages/NewProductsPage/NewProductsPage.js
@@ -7,31 +7,114 @@ function NewProductsPage() {
         <div className='newPage'>
             <div className='newPage-wrapper'>
 
-                <div className='card'>
+                <div className='card no-margin'>
                 <Link to='/processors'>
-                    <img src={require('../../../../public/assets/processors.jpg')} alt='mouse' className='card-img'/>
-                        <p className='card-text'>
-                            Shop Processors
-                        </p>
+                    <img src={require('../../../../public/assets/cpus.jpg')} alt='processors' className='card-img'/>
+                        
                 </Link>
+                <p className='card-text'>
+                           Processors
+                        </p>
                 </div>
                 <div className='card'>
                     <Link to='graphics-cards' >
-                        <img src={require('../../../../public/assets/gpus.jpg')} alt='' className='card-img'/>
-                        <p className='card-text'>
-                            Shop GPUs
-                        </p>
+                        <img src={require('../../../../public/assets/videocards.jpg')} alt='' className='card-img'/>
+                        
                     </Link>
+                    <p className='card-text'>
+                            Graphics Cards
+                    </p>
                 </div>
                 <div className='card'>
                     <Link to='ram'>
-                        <img src={require('../../../../public/assets/ram.jpg')} alt='' className='card-img'/>
-                        <p className='card-text'>
-                            Shop RAM
-                        </p>
+                        <img src={require('../../../../public/assets/ramModules.jpg')} alt='' className='card-img'/>
+                        
                     </Link>  
+                    <p className='card-text'>
+                            Memory
+                        </p>
                 </div>
-                
+                <div className='card'>
+                    <Link to='ram'>
+                        <img src={require('../../../../public/assets/motherboards.jpg')} alt='' className='card-img'/>
+                       
+                    </Link> 
+                    <p className='card-text'>
+                            Motherboards
+                        </p> 
+                </div>
+                <div className='card'>
+                    <Link to='ram'>
+                        <img src={require('../../../../public/assets/monitors.jpg')} alt='' className='card-img'/>
+                        
+                    </Link>
+                    <p className='card-text'>
+                            Monitors
+                        </p>  
+                </div>
+                <div className='card'>
+                    <Link to='ram'>
+                        <img src={require('../../../../public/assets/ssds.jpg')} alt='' className='card-img'/>
+                        
+                    </Link>
+                    <p className='card-text'>
+                            Hard Drives and SSDs
+                        </p>  
+                </div>
+                <div className='card'>
+                    <Link to='ram'>
+                        <img src={require('../../../../public/assets/coolers.jpg')} alt='' className='card-img'/>
+                        
+                    </Link>
+                    <p className='card-text'>
+                            Coolers
+                        </p>  
+                </div>
+                <div className='card'>
+                    <Link to='ram'>
+                        <img src={require('../../../../public/assets/routers.jpg')} alt='' className='card-img'/>
+                        
+                    </Link>  
+                    <p className='card-text'>
+                            Routers
+                        </p>
+                </div>
+                <div className='card'>
+                    <Link to='ram'>
+                        <img src={require('../../../../public/assets/powerSupplys.jpg')} alt='' className='card-img'/>
+                        
+                    </Link>
+                    <p className='card-text'>
+                            Power Supplies
+                        </p>  
+                </div>
+                <div className='card'>
+                    <Link to='ram'>
+                        <img src={require('../../../../public/assets/cases.jpg')} alt='' className='card-img'/>
+                        
+                    </Link> 
+                    <p className='card-text'>
+                            Cases
+                        </p> 
+                </div>
+                <div className='card'>
+                    <Link to='ram'>
+                        <img src={require('../../../../public/assets/desktops.jpg')} alt='' className='card-img'/>
+                        
+                    </Link>
+                    <p className='card-text'>
+                            Desktops
+                        </p>  
+                </div>
+                <div className='card'>
+                    <Link to='ram'>
+                        <img src={require('../../../../public/assets/laptops.jpg')} alt='' className='card-img'/>
+                        
+                    </Link>
+                    <p className='card-text'>
+                            Laptops
+                        </p>  
+                </div>
             </div>
         </div>
     )
