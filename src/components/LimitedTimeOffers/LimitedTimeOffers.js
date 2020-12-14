b6479b62fe71a3f85201353877a998b2751fe3f6
aaronleon1 <aaron.leon21@gmail.com>
2020-12-13 20:55:28 -0500

    added home product image resized and carousel limited to one slide on mobile

--- src/components/LimitedTimeOffers/LimitedTimeOffers.js
+++ src/components/LimitedTimeOffers/LimitedTimeOffers.js
@@ -4,7 +4,7 @@ import Carousel from 'react-elastic-carousel'
 
 function LimitedTimeOffers(){
     const breakPoints = [
-        { width: 1, itemsToShow: 2, itemsToScroll: 2 },
+        { width: 1, itemsToShow: 1},
         { width: 550, itemsToShow: 2, itemsToScroll: 2 },
         { width: 768, itemsToShow: 4 },
         { width: 1200, itemsToShow: 4, itemsToScroll: 4 }
