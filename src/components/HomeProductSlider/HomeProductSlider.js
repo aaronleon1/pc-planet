60915ce84d0828c8b4cdf266dca0b6b2b1d07617
aaronleon1 <aaron.leon21@gmail.com>
2020-12-17 02:37:02 -0500

    mobile modal styling and links added to home product carousel

--- src/components/HomeProductSlider/HomeProductSlider.js
+++ src/components/HomeProductSlider/HomeProductSlider.js
@@ -6,18 +6,18 @@ import './HomeProductSlider.css'
 function HomeProductSlider(){
         let items = [
             
-            {src: 'cpus.jpg', name: 'PROCESSORS'},
-            {src: 'videocards.jpg', name: 'GRAPHICS CARDS'},
-            {src: 'ramModules.jpg', name: 'MEMORY'},
-            {src: 'motherboards.jpg', name: 'MOTHERBOARDS'},
-            {src: 'monitors.jpg', name: 'MONITORS'},
-            {src: 'ssds.jpg', name: 'SSDs'},
-            {src: 'coolers.jpg', name: 'COOLERS'},
-            {src: 'routers.jpg', name: 'ROUTERS'},
-            {src: 'powerSupplys.jpg', name: 'POWER SUPPLIES'},
-            {src: 'cases.jpg', name: 'CASES'},
-            {src: 'desktops.jpg', name: 'DESKTOPS'},
-            {src: 'laptops.jpg', name: 'LAPTOPS'},
+            {src: 'cpus.jpg', name: 'PROCESSORS', url: 'processors'},
+            {src: 'videocards.jpg', name: 'GRAPHICS CARDS', url: 'graphics-cards'},
+            {src: 'ramModules.jpg', name: 'MEMORY', url: 'ram'},
+            {src: 'motherboards.jpg', name: 'MOTHERBOARDS', url: 'motherboards'},
+            {src: 'monitors.jpg', name: 'MONITORS', url: 'monitors'},
+            {src: 'ssds.jpg', name: 'SSDs', url: 'ssds'},
+            {src: 'coolers.jpg', name: 'COOLERS', url: 'coolers'},
+            {src: 'routers.jpg', name: 'ROUTERS', url: 'routers'},
+            {src: 'powerSupplys.jpg', name: 'POWER SUPPLIES', url: 'power-supplies'},
+            {src: 'cases.jpg', name: 'CASES', url: 'computer-cases'},
+            {src: 'desktops.jpg', name: 'DESKTOPS', url: 'desktops'},
+            {src: 'laptops.jpg', name: 'LAPTOPS', url: 'laptops'},
         ]
 
         let breakPoints=[
@@ -35,7 +35,7 @@ function HomeProductSlider(){
                     items.map((category) => {
                         return (
                             <div className='category-wrapper'>
-                                <Link to='/products'>
+                                <Link to={'/' + category.url}>
                                     <img src={require(`../../../public/assets/${category.src}`)} alt={category.name} className='category-img' />
                                 </Link>
                                 <p className='category-name'>{category.name}</p>
