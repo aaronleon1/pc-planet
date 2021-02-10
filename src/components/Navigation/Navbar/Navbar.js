896fdea151345744c6f88d504275d361572abfb0
aaron.leon21 <aaron.leon21@gmail.com>
2021-02-10 04:13:23 -0500

    navbar and footer now fitting within 1600px max width for site content

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -27,58 +27,61 @@ const Navbar = () => {
     }
 
     return (
-        <nav className='navbar'>
-            <div className='menu-icon' onClick={handleClick}>
-                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        <nav className='navbar'> 
+            <div className='nav-container'>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <Link to='/' className='navbar-logo'>
+                    PCPlanet<i className="fas fa-memory" />
+                </Link>
+                <Link to='/cart' className='cart-icon' onClick={closeMobileMenu} >
+                    <i className='fas fa-shopping-cart' />
+                </Link>
+                <Link to='' className='user-icon' onClick={closeMobileMenu} >
+                    <i class="far fa-user"></i>
+                </Link>
+                <ul className={click ? 'nav-menu active': 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item' 
+                        onMouseEnter={onMouseEnter}
+                        onMouseLeave={onMouseLeave}
+                    >
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                            Products <i className='fas fa-caret-down' />
+                        </Link>
+                        {dropdown && <Dropdown />}
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
+                            Contact Us
+                        </Link>
+                    </li>
+                    
+                    <li className='nav-item action-links' >
+                        <Link to='' className='nav-links' onClick={closeMobileMenu} >
+                        <i class="fas fa-search"></i>
+                        </Link>
+                    </li>
+                    <li className='nav-item action-links' >
+                        <Link to='' className='nav-links' onClick={closeMobileMenu} >
+                            <i class="far fa-user"></i>
+                        </Link>
+                    </li>
+                    <li className='nav-item action-links' >
+                        <Link to='/cart' className='nav-links' onClick={closeMobileMenu} >
+                            <i className='fas fa-shopping-cart' />
+                        </Link>
+                    </li>
+                    
+                    
+                </ul>
             </div>
-            <Link to='/' className='navbar-logo'>
-                PCPlanet<i className="fas fa-memory" />
-            </Link>
-            <Link to='/cart' className='cart-icon' onClick={closeMobileMenu} >
-                <i className='fas fa-shopping-cart' />
-            </Link>
-            <Link to='' className='user-icon' onClick={closeMobileMenu} >
-                <i class="far fa-user"></i>
-            </Link>
-            <ul className={click ? 'nav-menu active': 'nav-menu'}>
-                <li className='nav-item'>
-                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                        Home
-                    </Link>
-                </li>
-                <li className='nav-item' 
-                    onMouseEnter={onMouseEnter}
-                    onMouseLeave={onMouseLeave}
-                >
-                    <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
-                        Products <i className='fas fa-caret-down' />
-                    </Link>
-                    {dropdown && <Dropdown />}
-                </li>
-                <li className='nav-item'>
-                    <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
-                        Contact Us
-                    </Link>
-                </li>
-                
-                <li className='nav-item action-links' >
-                    <Link to='' className='nav-links' onClick={closeMobileMenu} >
-                    <i class="fas fa-search"></i>
-                    </Link>
-                </li>
-                <li className='nav-item action-links' >
-                    <Link to='' className='nav-links' onClick={closeMobileMenu} >
-                        <i class="far fa-user"></i>
-                    </Link>
-                </li>
-                <li className='nav-item action-links' >
-                    <Link to='/cart' className='nav-links' onClick={closeMobileMenu} >
-                        <i className='fas fa-shopping-cart' />
-                    </Link>
-                </li>
-                
-                
-            </ul>
+            
             
         </nav>
     )
